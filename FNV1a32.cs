cdac0a5d6e33bcaf5306746f0fe85bd85f9f7f05
Narazaka <info@narazaka.net>
2024-03-13 17:40:43 +0900

    typo

--- FNV1a32.cs
+++ FNV1a32.cs
@@ -2,7 +2,7 @@ namespace Narazaka.SimpleHash
 {
     public static class FNV1a32
     {
-        // cvf. https://en.wikipedia.org/wiki/Fowler%E2%80%93Noll%E2%80%93Vo_hash_function
+        // cf. https://en.wikipedia.org/wiki/Fowler%E2%80%93Noll%E2%80%93Vo_hash_function
         const uint FNV_OFFSET_BASIS = 2166136261;
         const uint FNV_PRIME = 16777619;
         public static uint ComputeHash(byte[] bytes)
