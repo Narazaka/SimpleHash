cf43656ed8e12b32eed4caf480cd771b9d96687c
Narazaka <info@narazaka.net>
2024-04-03 17:44:58 +0900

    HAS_UDON_ENCODING

--- UdonEncoding/FNV1a32String.cs
+++ UdonEncoding/FNV1a32String.cs
@@ -2,9 +2,11 @@ namespace Narazaka.SimpleHash
 {
     public static class FNV1a32String
     {
+#if HAS_UDON_ENCODING
         public static uint ComputeHash(string str)
         {
             return FNV1a32.ComputeHash(Koyashiro.UdonEncoding.UdonUTF8.GetBytes(str));
         }
+#endif
     }
 }
